0a42f4d700bfde1153ec0094590680cc5f245b6f
TierSnow <mehdi.jam@efrei.net>
2025-11-07 09:16:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 
 int main() {
+        //test the functions of part 1 with the file exemple1.txt.
+        //Note that, as your path will likely be different from the one here, it is necessary to change "C:/Users/USER/Downloads" to where you stored the project.
+        //Absolute path is necessary for the project to work
         a_list list;
         list = readGraph("C:/Users/USER/Downloads/TI_301_PRJ_STUDENTS-master/data/exemple1.txt");
         display_a_list(&list);
