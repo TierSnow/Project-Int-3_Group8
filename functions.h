76e57b12e986710d3e9897ba5aa36c82889d398f
TierSnow <mehdi.jam@efrei.net>
2025-11-23 04:03:24 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -26,6 +26,47 @@ typedef struct {
     int size;
 } a_list;
 
+typedef struct {
+    int identifier;     // Node number in the graph
+    int number;         // Temporary numbering for Tarjan
+    int acc_number;     // Accessible number for grouping
+    int indic;          // Boolean for stack membership (0 or 1)
+} t_tarjan_vertex;
+
+// Array for tracking state of ALL vertices in the graph
+typedef struct {
+    t_tarjan_vertex *vertices;  // Dynamic array
+    int size;                   // Current number of vertices
+    int capacity;               // Maximum capacity
+} t_tarjan_array;
+
+// Class to store grouped vertices
+typedef struct {
+    char *name;                 // Class name: "C1", "C2", etc.
+    t_tarjan_vertex *vertices;  // Dynamic array of vertices in this class
+    int size;                   // Current number of vertices
+    int capacity;               // Maximum capacity
+} t_class;
+
+// Partition storing all classes
+typedef struct {
+    t_class *classes;           // Dynamic array of classes
+    int size;                   // Current number of classes
+    int capacity;               // Maximum capacity
+} t_partition;
+
+typedef struct {
+    int *data;      // Array to store complete vertices
+    int top;                    // Index of top element
+    int capacity;               // Maximum capacity
+} t_stack;
+
+// Matrix structure definition
+typedef struct {
+    float **data;   // 2D array for matrix data
+    int size;       // Matrix size (n x n)
+} matrix;
+
 
 cell *create_cell(int, float);
 
@@ -45,4 +86,35 @@ int check_graph(const char *);
 
 void export_graph(a_list *, const char *);
 
+t_tarjan_array* init_tarjan_vertices(a_list *);
+
+// Push a vertex identifier onto the stack
+void push(t_stack *, int);
+
+// Pop a vertex identifier from the stack
+int pop(t_stack *);
+
+t_stack* create_stack(int);
+
+void parcours(int , a_list *, t_tarjan_array *, t_stack *, int *, t_partition *);
+
+void tarjan(a_list *);
+
+t_partition compute_partition(a_list *);
+
+int** Hasse(a_list *, t_partition *);
+
+void analyze_graph_characteristics(t_partition *, int **, int);
+
+matrix* create_transition_matrix(a_list *);
+
+matrix* create_zero_matrix(int);
+
+void copy_matrix(matrix *, matrix *);
+
+matrix* multiply_matrices(matrix *, matrix *);
+
+float matrix_difference(matrix *, matrix *);
+
+matrix* subMatrix(matrix *, t_partition *, int);
 #endif //FUNCTIONS_H
